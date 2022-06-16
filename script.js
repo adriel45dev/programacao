bf4286a395db9a459848d2644c008c012f1ece07
Adriel Alves <98328671+adriel45dev@users.noreply.github.com>
2022-06-16 10:05:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,77 @@
+/**
+ * :.: REVISÃO - PROGRAMAÇÃO WEB :.:
+ * :.: JAVASCRIPT
+ * 1 - DECLARAÇÃO DE VARIÁVEIS
+ * 2 - DOM
+ * 3 - FUNÇÕES
+ * 4 - OPERADORES DE COMPARAÇÃO (COMPARAÇÃO BOOLEANA)
+ * 5 - OPERADOR CONDICINAL IF...ELSE
+ * 6 - ARRAY (LISTA)
+ *  6.1 - POP e PUSH
+ *  6.2 - SELECIONAR UM ELEMENTO DA LISTA
+ * 
+ **/
+
+
+/* função */
+function cliqueMe(){
+    alert("Você clicou no botão");
+}
+
+/* tipos de dados */
+var idade = 19;
+var nome = "Carla";
+var ano = "2022"; 
+var verdadeiro = true;
+
+
+/* DOM */
+var btn = document.querySelector("button");
+
+btn.addEventListener("click", cliqueMe);
+
+/* var btn é a declaração de uma variável
+
+*/
+
+/* Selecinar o botão com id c */
+function umaFuncao(){
+    alert("Você clicou no botão C")
+}
+
+document.querySelector("#c").addEventListener("click", umaFuncao);
+
+
+/* Arrays */
+var vogais = ['a', 'e'];
+
+vogais[1]; // acessar um elemento da lista
+
+vogais.push('i');
+vogais.pop();
+
+// insert e remove
+
+/* ex: laço */
+var M = 20;
+var N = 40;
+
+/* repetir 4 vezes */
+for(var i = 1; i<=4; i++){
+    M = M - 2;
+    N = N - 2;
+}
+
+/* comparações lógicas */
+(2*5) == 10; /* 10 == 10 VERDADE*/ 
+(1 < 3) && (2 == 2); /* V && V = VERDADE */
+((1 != 2) && (5 < 2)) || 4 > 3; 
+
+/* if .. else | comparadores lógicos  */
+var num1 = 10, num2 = 5;
+
+if(num1 > num2){
+    alert("O primeiro número é maior");
+} 
+
+ 
