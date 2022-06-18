cc137b13859af90ba94fcf073e95e0b4764cafc5
Adriel Alves <98328671+adriel45dev@users.noreply.github.com>
2022-06-18 12:40:30 -0300

    Update programacao_web_rec.js

--- programacao_web_rec.js
+++ programacao_web_rec.js
@@ -5,12 +5,43 @@
  * 2 - DOM
  * 3 - FUNÇÕES
  * 4 - OPERADORES DE COMPARAÇÃO (COMPARAÇÃO BOOLEANA)
+ *     >, <, !=, ==
  * 5 - OPERADOR CONDICINAL IF...ELSE
  * 6 - ARRAY (LISTA)
  *  6.1 - POP e PUSH
  *  6.2 - SELECIONAR UM ELEMENTO DA LISTA
  * 
  **/
+
+/***
+Declarações
+Tipos de declaração
+    * const, var e let
+ Variáveis e constantes 
+    * o que é, e não é permitido
+
+Condicional IF
+ * operadores de comparação
+     >, <, !=, ==
+
+Operadores lógicos
+ E (&&), OU (||)
+* valores booleanos
+
+Array (Lista)
+- Manipular elementos de uma lista
+- Métodos: pop e push
+
+Função
+- Chamada da função 
+
+IMPORTANTE:
+* saber criar uma função
+* saber declarar uma variável
+* saber pedir informação
+* saber os operadores matemáticos básicos
+* saber selecionar um elemento com DOM
+*/
 
 
 /* função */
@@ -74,4 +105,14 @@ if(num1 > num2){
     alert("O primeiro número é maior");
 } 
 
+/* comparação booleana */
+var v1 = 3;
+var v2, v3;
+
+v2 = +prompt("Digite o número 3:")
+v3 = false;
+
+console.log(v1 == v2 ); // true
+console.log(v2 == v3); // false
+console.log(v1); // 3
  
